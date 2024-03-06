abbbca9a8b5b5afa3d2e085df69091115a7550c3
Jordan Mena <jsmena11@gmail.com>
2024-03-06 05:01:19 +0000

    Changes to config to run on linux. Updated pillow, jinja, and blinker

--- home_page/config.py
+++ home_page/config.py
@@ -1,11 +1,14 @@
 import os
+import json
 
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = config.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smpt.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = config.get('MAIL_USERNAME')
+    MAIL_PASSWORD = config.get('MAIL_PASSWORD')

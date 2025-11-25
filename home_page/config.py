c062c1f000278976f521829f0def57e1922c5325
JMena <jsmena11@gmail.com>
2025-11-24 21:36:40 -0700

    feat: Introduce database models, application configuration, a database verification script, and enable debug mode.

--- home_page/config.py
+++ home_page/config.py
@@ -2,8 +2,9 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'cde00bf9b89d81a8e34d7b2eb9f8bfbf'
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'site.db')
     MAIL_SERVER = 'smpt.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
